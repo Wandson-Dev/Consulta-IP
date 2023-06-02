b8d2f012f30fb8396dea52400f99adc1464b55ad
Wandson Developer <134511990+Wandson-Dev@users.noreply.github.com>
2023-06-02 09:56:20 -0300

    Melhorias no instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -10,29 +10,31 @@ reset='\033[0m'
 echo -e "${amarelo}[ ! ] ANTES DE BAIXAR AS DEPENDÊNCIAS NECESSÁRIAS, PRECISO DA SUA PERMISSÃO PARA INSTALAR AS COISAS NECESSÁRIAS PARA ESTE SCRIPT FUNCIONAR PERFEITAMENTE.${reset}"
 
 # Pede a permissão em ciano
-echo -e "${ciano}[ ? ] Permite a Instalação? Digite \"y\" Para Sim e \"n\" para Não. (y/n) \n"
+echo -e "${ciano}[ ? ] Permite a Instalação? Digite \"y\" Para Sim e \"n\" para Não. (y/n)"
 read resposta
 
 # Verifica a resposta do usuário
 if [[ $resposta == "y" || $resposta == "Y" ]]; then
     echo -e "${ciano}[ ! ] Tenha Paciência, Pode Demorar Variando da Sua Internet e do Servidor${reset}"
-
+    echo 
     echo -e "${amarelo}[ ! ] INSTALANDO DEPENDÊNCIAS, AGUARDE... (0%)${reset}"
     pkg install git -y > /dev/null 2>&1
-
+    echo 
     echo -e "${amarelo}[ ! ] INSTALANDO DEPENDÊNCIAS, AGUARDE... (25%)${reset}"
     pkg install python3 -y > /dev/null 2>&1
-
+    echo 
     echo -e "${amarelo}[ ! ] INSTALANDO DEPENDÊNCIAS, AGUARDE... (50%)${reset}"
     pkg install nano -y > /dev/null 2>&1
-
+    echo 
     echo -e "${amarelo}[ ! ] INSTALANDO DEPENDÊNCIAS, AGUARDE... (75%)${reset}"
     pip install requests > /dev/null
-
+    echo 
     echo -e "${amarelo}[ ! ] INSTALANDO DEPENDÊNCIAS, AGUARDE... (99%)${reset}"
     pip install termcolor > /dev/null
-
+    echo 
+    echo 
     echo -e "${amarelo}[ ✔️ ] DEPENDÊNCIAS INSTALADAS, DE python3 start.py PARA CONTINUAR. ${reset}"
+    echo 
     # Adicione aqui a lógica para instalar as dependências
 elif [[ $resposta == "n" || $resposta == "N" ]]; then
     echo -e "${amarelo}Instalação cancelada.${reset}"
